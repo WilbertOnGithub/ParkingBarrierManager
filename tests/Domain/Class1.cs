adc871b472a3ab2b69f4662ebed506301a5326bc
Wilbert van Dolleweerd <wilbert@arentheym.com>
2023-07-25 10:32:34 +0200

    Add assembly names

--- tests/Domain/Class1.cs
+++ tests/Domain/Class1.cs
@@ -1,6 +1,17 @@
 ﻿namespace Arentheym.ParkingBarrier.Domain.Tests;
 
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+
 public class Class1
 {
+    private readonly IFixture fixture;
 
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Class1()
+    {
+        fixture = new Fixture().Customize(new AutoNSubstituteCustomization());
+    }
 }

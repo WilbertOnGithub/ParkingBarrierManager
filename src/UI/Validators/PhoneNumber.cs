39a8cab43533587ef688e302acc6188e8bf7bd80
Wilbert van Dolleweerd <wilbert@arentheym.com>
2023-08-02 11:50:20 +0200

    Add validator for PhoneNumber

---
+++ src/UI/Validators/PhoneNumber.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Arentheym.ParkingBarrier.UI.Validators;
+
+public sealed partial class PhoneNumber : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (PhoneNumberRegEx().IsMatch(value as string ?? string.Empty))
+        {
+            return ValidationResult.Success;
+        }
+
+        return new("Phonenumber can be empty or a number of exactly 10 digits (ignoring whitespace).");
+    }
+
+    /// <summary>
+    /// A regular expression that matches a phone number where the phone number is exactly 10 digits long.
+    /// Whitespace in the number is allowed. The number can also be empty.
+    /// </summary>
+    [GeneratedRegex(@"^(?:\s*\d\s*){10}$|^$", RegexOptions.Compiled)]
+    private static partial Regex PhoneNumberRegEx();
+}
--- tests/UiTests/ValidatorTests.cs
+++ tests/UiTests/ValidatorTests.cs
@@ -1,35 +1,28 @@
-﻿using Arentheym.ParkingBarrier.UI.Validators;
+﻿using System.Diagnostics.CodeAnalysis;
+using Arentheym.ParkingBarrier.UI.Validators;
 
 using Xunit;
 using FluentAssertions;
 
 namespace Arentheym.ParkingBarrier.UI.Tests;
 
+[SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Unit testing naming")]
 public class ValidatorTests
 {
-    [Fact]
-    public void AccessCodeCanBeEmpty()
-    {
-        // Arrange
-        AccessCode accessCode = new();
-
-        // Assert / Act
-        accessCode.IsValid(string.Empty).Should().BeTrue();
-    }
-
     [Theory]
+    [InlineData("")]
     [InlineData("1")]
     [InlineData("12")]
     [InlineData("123")]
     [InlineData("1234")]
     [InlineData("12345")]
     [InlineData("123456")]
-    public void AccessCodeCanContainOneToSixDigits(string data)
+    public void AccessCode_Is_Valid_With_Valid_Data(string data)
     {
         // Arrange
         AccessCode accessCode = new();
 
-        // Assert / Act
+        // Act / Assert
         accessCode.IsValid(data).Should().BeTrue();
     }
 
@@ -37,12 +30,39 @@ public class ValidatorTests
     [InlineData("A")]
     [InlineData("1234567")]
     [InlineData("1234-567")]
-    public void AccessCodeCannotContainInvalidData(string data)
+    public void AccessCode_Is_InValid_With_Invalid_Data(string data)
     {
         // Arrange
         AccessCode accessCode = new();
 
-        // Assert / Act
+        // Act / Assert
         accessCode.IsValid(data).Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("1234567890")]
+    [InlineData(" 1 2 3 4 5 6 7 8 9 0 ")]
+    public void PhoneNumber_Is_Valid_With_Valid_Data(string data)
+    {
+        // Arrange
+        PhoneNumber phoneNumber = new();
+
+        // Act / Assert
+        phoneNumber.IsValid(data).Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("abc")]             // No digits
+    [InlineData("12345678901")]     // Too long
+    [InlineData("123456")]          // Too short
+    [InlineData(" 1 2 3 4 5 6 ")]   // Too short with whitespace
+    public void PhoneNumber_Is_Invalid_With_Invalid_Data(string data)
+    {
+        // Arrange
+        PhoneNumber phoneNumber = new();
+
+        // Act / Assert
+        phoneNumber.IsValid(data).Should().BeFalse();
     }
 }

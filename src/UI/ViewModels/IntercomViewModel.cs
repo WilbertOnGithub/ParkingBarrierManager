56e1d8c0ad8423de2e21a5383fd618c82f3d2eed
Wilbert van Dolleweerd <wilbert@arentheym.com>
2023-08-02 17:03:19 +0200

    Add viewmodel for intercoms

---
+++ src/UI/ViewModels/IntercomViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Arentheym.ParkingBarrier.UI.ViewModels;
+
+/// <summary>
+/// Represents the intercom to which a <see cref="ApartmentConfigurationViewModel"/>
+/// is linked.
+/// </summary>
+public partial class IntercomViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private bool isUsed;
+
+    private string name;
+
+    public string Name
+    {
+        get => name;
+        init => SetProperty(ref name, value);
+    }
+
+    public IntercomViewModel()
+    {
+        name = string.Empty;
+    }
+}

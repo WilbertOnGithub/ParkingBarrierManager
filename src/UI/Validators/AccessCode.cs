d4547a5930e3ad7d37e62d2686b1246cd2ddcb6b
Wilbert van Dolleweerd <wilbert@arentheym.com>
2023-08-02 09:59:28 +0200

    Add initial viewmodels

---
+++ src/UI/Validators/AccessCode.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Arentheym.ParkingBarrier.UI.Validators;
+
+public sealed partial class AccessCode : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (AccessCodeRegEx().IsMatch(value as string ?? string.Empty))
+        {
+            return ValidationResult.Success;
+        }
+
+        return new("Access code must be empty or a number between 1 and 6 digits.");
+    }
+
+    [GeneratedRegex(@"\d{1,6}$|^$", RegexOptions.Compiled | RegexOptions.Singleline)]
+    private static partial Regex AccessCodeRegEx();
+}
---
+++ src/UI/ViewModels/ApartmentConfigurationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Arentheym.ParkingBarrier.UI.Validators;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Arentheym.ParkingBarrier.UI.ViewModels;
+
+public partial class ApartmentConfigurationViewModel : ObservableValidator
+{
+    [ObservableProperty]
+    private string primaryPhoneNumber;
+
+    [ObservableProperty]
+    private string secondaryPhoneNumber;
+
+    [ObservableProperty]
+    private string tertiaryPhoneNumber;
+
+    [ObservableProperty]
+    private string quaternaryPhoneNumber;
+
+    [AccessCode]
+    private string accessCode;
+
+    [Required]
+    private bool dialToOpen;
+
+    [Required]
+    [MaxLength(16)]
+    private string displayName;
+
+    public int ApartmentNumber { get; init; }
+
+    public ApartmentConfigurationViewModel()
+    {
+        primaryPhoneNumber = string.Empty;
+        secondaryPhoneNumber = string.Empty;
+        tertiaryPhoneNumber = string.Empty;
+        quaternaryPhoneNumber = string.Empty;
+        accessCode = string.Empty;
+        displayName = string.Empty;
+        dialToOpen = false;
+    }
+}

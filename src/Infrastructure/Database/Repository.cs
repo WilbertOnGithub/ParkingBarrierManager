6910ce316aba149ea82b3ebdcfbeb373b26cb7ee
Wilbert van Dolleweerd <wilbert.arentheym.com>
2023-08-07 16:43:50 +0200

    Update repository

---
+++ src/Infrastructure/Database/Repository.cs
@@ -0,0 +1,40 @@
+﻿using Arentheym.ParkingBarrier.Application;
+using Arentheym.ParkingBarrier.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Arentheym.ParkingBarrier.Infrastructure.Database;
+
+public class Repository : IRepository
+{
+    private readonly DatabaseContext databaseContext;
+    public Repository(DatabaseContext databaseContext)
+    {
+        this.databaseContext = databaseContext;
+    }
+
+    public async Task<IEnumerable<Intercom>> GetIntercomsAsync()
+    {
+        return await databaseContext.Intercoms.OrderBy(x => x.Name)
+                                              .AsNoTracking()
+                                              .ToListAsync()
+                                              .ConfigureAwait(false);
+    }
+
+    public async Task<IEnumerable<ApartmentConfiguration>> GetApartmentConfigurationsAsync()
+    {
+        return await databaseContext.ApartmentConfigurations
+                                    .Include(x => x.PhoneNumbers)
+                                    .Include(x => x.Intercoms)
+                                    .OrderBy(x => x.Id)
+                                    .AsNoTracking()
+                                    .ToListAsync()
+                                    .ConfigureAwait(false);
+    }
+
+    public async Task SaveApartmentConfigurations(IEnumerable<ApartmentConfiguration> modifiedApartmentConfigurations)
+    {
+        databaseContext.UpdateRange(modifiedApartmentConfigurations);
+
+        await databaseContext.SaveChangesAsync().ConfigureAwait(false);
+    }
+}

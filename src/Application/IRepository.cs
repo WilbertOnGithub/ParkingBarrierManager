b9cf0b26145cd04998f969284b992bbaff17fd53
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2023-08-07 16:43:50 +0200

    Update repository

---
+++ src/Application/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Arentheym.ParkingBarrier.Domain;
+
+namespace Arentheym.ParkingBarrier.Application;
+
+/// <summary>
+///
+/// </summary>
+public interface IRepository
+{
+    Task<IEnumerable<Intercom>> GetIntercomsAsync();
+
+    Task<IEnumerable<ApartmentConfiguration>> GetApartmentConfigurationsAsync();
+
+    Task SaveApartmentConfigurations(IEnumerable<ApartmentConfiguration> modifiedApartmentConfigurations);
+}

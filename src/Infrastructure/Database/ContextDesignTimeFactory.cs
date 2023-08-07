3633d7b9d58b3a1af8bb6150ec0d7ee0cb6985d5
Wilbert van Dolleweerd <wilbert.arentheym.com>
2023-08-07 13:53:14 +0200

    Add Design time functionality EF Core

---
+++ src/Infrastructure/Database/ContextDesignTimeFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Arentheym.ParkingBarrier.Infrastructure.Database;
+
+public class ContextDesignTimeFactory : IDesignTimeDbContextFactory<DatabaseContext>
+{
+    public DatabaseContext CreateDbContext(string[] args)
+    {
+        IConfiguration configuration = GetDevelopmentConfiguration();
+
+        var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+        optionsBuilder.UseSqlite(configuration.GetSection("DatabaseConfiguration")
+            .GetValue<string>("ConnectionString"));
+
+        return new DatabaseContext(optionsBuilder.Options);
+
+        IConfiguration GetDevelopmentConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile($"appsettings.{Environment.MachineName}.json", optional: false)
+                .Build();
+        }
+    }
+
+}

0ab1bf7bfcc828a979a719e8f161252731b39d1f
Wilbert van Dolleweerd <wilbert.arentheym.com>
2023-08-15 08:46:09 +0200

    Move resources to different folder

--- src/UI/Assets/Resources.nl-NL.Designer.cs
+++ src/UI/Assets/Resources.nl-NL.Designer.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Arentheym.Resources {
+namespace Arentheym.Assets {
     using System;
     
     
@@ -21,14 +21,14 @@ namespace Arentheym.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    public class Resources_nl_NL {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal Resources_nl_NL() {
         }
         
         /// <summary>
@@ -38,7 +38,7 @@ namespace Arentheym.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Arentheym.Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Arentheym.Assets.Resources.nl-NL", typeof(Resources_nl_NL).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -60,7 +60,7 @@ namespace Arentheym.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Parking Barrier Manager.
+        ///   Looks up a localized string similar to Slagboom beheer VvE de Grote Enk.
         /// </summary>
         public static string ApplicationTitle {
             get {

73f377c747ed71a71a7779daa5e311a4d2c16981
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2023-08-15 10:20:17 +0200

    Add button to save configuration

--- src/UI/ViewModels/MainWindowViewModel.cs
+++ src/UI/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading.Tasks;
 using Arentheym.ParkingBarrier.Application;
+using Arentheym.ParkingBarrier.Domain;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Arentheym.ParkingBarrier.UI.ViewModels;
 
@@ -9,23 +13,29 @@ public partial class MainWindowViewModel : ViewModelBase
 {
     private readonly DataService dataService;
     private readonly Task initialisationTask;
+    private IEnumerable<Intercom> availableIntercoms = Enumerable.Empty<Intercom>();
 
     public ObservableCollection<ApartmentConfigurationViewModel> Configurations { get; } = new ObservableCollection<ApartmentConfigurationViewModel>();
-
 
     [SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Dependency injection")]
     public MainWindowViewModel(DataService dataService)
     {
         this.dataService = dataService;
-        this.initialisationTask = InitializeAsync();
+        initialisationTask = InitializeAsync();
     }
 
     private async Task InitializeAsync()
     {
-        var intercoms = await dataService.GetIntercoms().ConfigureAwait(false);
+        availableIntercoms = await dataService.GetIntercoms().ConfigureAwait(false);
         foreach (var domainEntity in await dataService.GetApartmentConfigurations().ConfigureAwait(false))
         {
-            Configurations.Add(ManualMapper.EntityToViewModel(domainEntity, intercoms));
+            Configurations.Add(ManualMapper.EntityToViewModel(domainEntity, availableIntercoms));
         }
+    }
+
+    [RelayCommand]
+    public async Task SaveConfigurationsAsync()
+    {
+        await dataService.SaveApartmentConfigurations(Configurations.Select(x => ManualMapper.ViewModelToEntity(x, availableIntercoms))).ConfigureAwait(false);
     }
 }

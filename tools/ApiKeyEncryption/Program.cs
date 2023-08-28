a4395a19eee9a17d5bb3c4bfedca548f0fe6e6de
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2023-08-28 13:30:13 +0200

    Add Commandline parser

--- tools/ApiKeyEncryption/Program.cs
+++ tools/ApiKeyEncryption/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Arentheym.ParkingBarrier.UI;
+﻿using CommandLine;
+
+namespace Arentheym.ParkingBarrier.UI;
 
 public static class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(o =>
+            {
+                Console.WriteLine(o.StringToDecrypt);
+                Console.WriteLine(o.StringToEncrypt);
+            });
     }
 }

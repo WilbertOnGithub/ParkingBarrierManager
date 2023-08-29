09c0fd9cfc468679a04f1b2d664259e28d62da17
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2023-08-29 13:20:37 +0200

    Add encryption functionality

--- src/Application/Encryptor.cs
+++ src/Application/Encryptor.cs
@@ -3,30 +3,61 @@ using System.Text;
 
 namespace Arentheym.ParkingBarrier.Application;
 
-public class Encryptor
+public sealed class Encryptor : IDisposable
 {
-    public string Encrypt (string toEncrypt)
+    private readonly Aes aes;
+    private const int keyLength = 16;
+
+    public Encryptor()
     {
-        using var aes = Aes.Create();
+        aes = Aes.Create();
 
-        aes.Key = key;
-        byte[] iv = aes.IV;
-
-        MemoryStream outputStream = new MemoryStream();
-        outputStream.Write(iv, 0, iv.Length);
-
-        using CryptoStream cryptoStream =
-            new CryptoStream(outputStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
-        using StreamWriter encryptWriter = new StreamWriter(cryptoStream);
-        encryptWriter.Write(toEncrypt);
-
-        outputStream.Seek(9, SeekOrigin.Begin);
-        using StreamReader reader = new StreamReader(outputStream);
-        string text = reader.ReadToEnd();
-
-        byte[] toEncodeAsBytes = ASCIIEncoding.ASCII.GetBytes(text);
-        return Convert.ToBase64String(toEncodeAsBytes);
+        // Size is number of chars multiplied by 2 (2 bytes per char) multiplied by 8 (8 bits per byte) = 256 bits.
+        aes.KeySize = keyLength * 2 * 8;
+        aes.Key = GetPassPhrase();
+        aes.GenerateIV();
     }
 
-    private byte[] key = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+    public string Encrypt (string plainText)
+    {
+        byte[] encrypted;
+
+        using (var ms = new MemoryStream())
+        {
+            // Prepend the generated initialization vector to the encrypted bytes.
+            ms.Write(aes.IV, 0, 16); ;
+            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                using (var sw = new StreamWriter(cs))
+                {
+                    sw.Write(plainText);
+                }
+            }
+
+            encrypted = ms.ToArray();
+        }
+
+        return Convert.ToBase64String(encrypted);
+    }
+
+    private static byte[] GetPassPhrase()
+    {
+        var key = "TheQuickBrownFox";
+        if (key.Length < keyLength)
+        {
+            key += new string('0', keyLength - key.Length);
+        }
+
+        if (key.Length > keyLength)
+        {
+            throw new InvalidOperationException("Key must be 16 characters long.");
+        }
+
+        return Encoding.UTF8.GetBytes(key);
+   }
+
+    public void Dispose()
+    {
+        aes.Dispose();
+    }
 }

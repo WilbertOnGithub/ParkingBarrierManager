0b4dfebe56d3debc89bc33fb1913f3c067b4daa7
Wilbert van Dolleweerd <wilbert@arentheym.com>
2023-08-31 15:16:54 +0200

    Only read appsettings once

--- src/Application/ApplicationServicesExtension.cs
+++ src/Application/ApplicationServicesExtension.cs
@@ -1,10 +1,13 @@
-﻿namespace Arentheym.ParkingBarrier.Application;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
+
+namespace Arentheym.ParkingBarrier.Application;
 
 using Microsoft.Extensions.DependencyInjection;
 
 public static class ApplicationServicesExtension
 {
-    public static void RegisterApplicationServices(this IServiceCollection services)
+    public static void RegisterApplicationServices(this IServiceCollection services, [NotNull]IConfiguration configuration)
     {
         services.AddTransient<DataService>();
     }

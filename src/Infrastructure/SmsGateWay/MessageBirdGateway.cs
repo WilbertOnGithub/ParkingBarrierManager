e57469591def348608f66f2c44cf83d8aa4e95f7
Wilbert van Dolleweerd <wilbert.arentheym.com>
2023-09-04 15:37:17 +0200

    Send SMS to Gateway

--- src/Infrastructure/SmsGateWay/MessageBirdGateway.cs
+++ src/Infrastructure/SmsGateWay/MessageBirdGateway.cs
@@ -1,4 +1,7 @@
-﻿using Arentheym.ParkingBarrier.Application;
+﻿using System.Diagnostics;
+using System.Globalization;
+using Arentheym.ParkingBarrier.Application;
+using Arentheym.ParkingBarrier.Domain;
 using FluentResults;
 using MessageBird;
 using MessageBird.Exceptions;
@@ -15,9 +18,49 @@ public class MessageBirdGateway : ISmsGateway
         client = Client.CreateDefault(apiKey);
     }
 
-    public Result Send()
+    public IList<Result> SendSms(ApartmentConfiguration apartmentConfiguration)
     {
-        return Result.Ok();
+        ArgumentNullException.ThrowIfNull(apartmentConfiguration);
+        List<Result> results = new();
+
+        // Create a separate SMS for each intercom.
+        foreach (var intercom in apartmentConfiguration.Intercoms)
+        {
+            long[] phoneNumber = { Convert.ToInt64(intercom.PhoneNumber.Number, CultureInfo.InvariantCulture) };
+            string originator = "MessageBird";
+            string body = new UpdateConfigurationCommand(
+                intercom.MasterCode, apartmentConfiguration).ToString();
+
+            try
+            {
+                Message message = client.SendMessage(originator, body, phoneNumber);
+                results.Add(Result.Ok());
+            }
+            catch (ErrorException e)
+            {
+                var errors = new List<IError>();
+
+                // Either the request fails with error descriptions from the endpoint.
+                if (e.HasErrors)
+                {
+                    foreach (MessageBird.Objects.Error error in e.Errors)
+                    {
+                        errors.Add(new FluentResults.Error($"Code: {error.Code} Description: {error.Description} Parameter: {error.Parameter}"));
+                    }
+                }
+
+                // Or it fails without error information from the endpoint,
+                // in which case the reason contains a 'best effort' description.
+                if (e.HasReason)
+                {
+                    errors.Add(new FluentResults.Error(e.Reason));
+                }
+
+                results.Add(Result.Fail(errors));
+            }
+        }
+
+        return results;
     }
 
     public Result<string> GetBalance()

bc277fbd7b77aa5dc324cc564f7753cc5c241cbb
Wilbert van Dolleweerd <wilbert@arentheym.com>
2023-09-07 11:08:14 +0200

    Add extra view/viewmodel
    To be used for the statusbar that will show
    the remaining credits.

---
+++ src/UI/ViewModels/StatusBarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Arentheym.ParkingBarrier.UI.ViewModels;
+
+public partial class StatusBarViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string text;
+
+    public StatusBarViewModel()
+    {
+        text = "Foobar";
+    }
+}
---
+++ src/UI/Views/StatusBarView.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Arentheym.ParkingBarrier.UI.ViewModels;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.DependencyInjection;
+
+namespace Arentheym.Views;
+
+public partial class StatusBarView : UserControl
+{
+    public StatusBarView()
+    {
+        InitializeComponent();
+        DataContext = Ioc.Default.GetRequiredService<StatusBarViewModel>();
+    }
+}

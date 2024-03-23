115cea86379d707366e41f81e6f46484cc2d5abf
Wilbert van Dolleweerd <wilbert@arentheym.com>
2024-03-23 14:07:17 +0100

    Extend coverage for DivertPhoneNumber

---
+++ tests/DomainTests/DivertPhoneNumberTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Arentheym.ParkingBarrier.Domain.Tests;
+
+[SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Unit testing naming")]
+public class DivertPhoneNumberTests
+{
+    [Fact]
+    public void DivertPhoneNumbers_can_be_equal()
+    {
+        // Arrange / Act
+        var left = new DivertPhoneNumber(DivertOrder.Primary, "31613739851");
+        var right = new DivertPhoneNumber(DivertOrder.Primary, "31613739851");
+
+        // Assert
+        left.Should().BeEquivalentTo(right, "because they are equal");
+    }
+}

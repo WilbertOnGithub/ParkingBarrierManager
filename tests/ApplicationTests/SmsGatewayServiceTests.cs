04f5a9144fef8c05f419f4dbe1c2b249124f1e8d
Wilbert van Dolleweerd <wilbert@arentheym.com>
2024-03-23 17:29:08 +0100

    Add tests for SmsGatewayService

--- tests/ApplicationTests/SmsGatewayServiceTests.cs
+++ tests/ApplicationTests/SmsGatewayServiceTests.cs
@@ -28,8 +28,9 @@ public class SmsGatewayServiceTests
     {
         // Arrange
         var smsGatewayMock = fixture.Freeze<ISmsGateway>();
-        List<IError> errors = new List<IError>() { new Error("Error") };
+        List<IError> errors = new() { new Error("Error") };
         smsGatewayMock.GetBalance().Returns(Result.Fail(errors));
+
         var sut = fixture.Create<SmsGatewayService>();
 
         // Act
@@ -37,5 +38,22 @@ public class SmsGatewayServiceTests
 
         // Assert
         result.Errors.Should().BeEquivalentTo(errors, "because the gateway failed");
+    }
+
+    [Fact]
+    public void SmsGatewayService_returns_success_when_SmsGateway_succeeds()
+    {
+        // Arrange
+        const string balance = "5 euro";
+        var smsGatewayMock = fixture.Freeze<ISmsGateway>();
+        smsGatewayMock.GetBalance().Returns(Result.Ok<string>(balance));
+        var sut = fixture.Create<SmsGatewayService>();
+
+        // Act
+        Result<string> result = sut.GetBalanceDetails();
+
+        // Assert
+        result.IsSuccess.Should().BeTrue("because the gateway succeeded");
+        result.Value.Should().Be(balance, "because the current credit balance was returned");
     }
 }

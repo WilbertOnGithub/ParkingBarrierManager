d58a89c27b00059d3fed9274d26b390b63c8edab
Wilbert van Dolleweerd <wilbert@arentheym.com>
2024-03-23 12:51:24 +0100

    Add tests for PhoneNumbers

--- tests/DomainTests/PhoneNumberTests.cs
+++ tests/DomainTests/PhoneNumberTests.cs
@@ -18,4 +18,30 @@ public class PhoneNumberTests
         // Assert
         act.Should().NotThrow<ArgumentException>("because this is a valid phone number.");
     }
+
+    [Fact]
+    public void An_empty_string_is_considered_to_be_an_empty_phonenumber()
+    {
+        // Arrange / Act
+        var phoneNumber = new PhoneNumber(string.Empty);
+
+        // Assert
+        phoneNumber.Should().Be(PhoneNumber.EmptyPhoneNumber);
+    }
+
+    [Theory]
+    [InlineData("abc")] // Phone numbers only contain digits
+    [InlineData("31613739851999999999")] // Valid country code (31) but invalid phone number (too long)
+    [InlineData("0000613739851")] // Invalid country code (0000) but valid phone number
+    public void PhoneNumber_with_invalid_data_throws_exception(string phoneNumber)
+    {
+        // Arrange / Act
+        Action act = () =>
+        {
+            _ = new PhoneNumber(phoneNumber);
+        };
+
+        // Assert
+        act.Should().Throw<ArgumentException>("because this is not a valid phone number.");
+    }
 }

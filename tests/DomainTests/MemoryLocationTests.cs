98752d66c00f2081b20727a67b601352a3ce6cd7
Wilbert van Dolleweerd <wilbert@arentheym.com>
2024-03-23 12:51:10 +0100

    Add test for invalid memorylocations

--- tests/DomainTests/MemoryLocationTests.cs
+++ tests/DomainTests/MemoryLocationTests.cs
@@ -5,15 +5,30 @@ public class MemoryLocationTests
 {
     [Theory]
     [ClassData(typeof(ValidMemoryLocationsTestData))]
-    public void MemoryLocation_must_be_between_0_and_499(short memoryLocation)
+    public void MemoryLocation_must_be_between_0_and_499(short validMemoryLocation)
     {
         // Arrange / Act
         Action act = () =>
         {
-            _ = new MemoryLocation(memoryLocation);
+            _ = new MemoryLocation(validMemoryLocation);
         };
 
         // Assert
         act.Should().NotThrow<ArgumentException>("because this is a valid memory location.");
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(500)]
+    public void Invalid_memory_location_throws_exception(short invalidMemoryLocation)
+    {
+        // Arrange / Act
+        Action act = () =>
+        {
+            _ = new MemoryLocation(invalidMemoryLocation);
+        };
+
+        // Assert
+        act.Should().Throw<ArgumentException>("because this is not a valid memory location.");
     }
 }

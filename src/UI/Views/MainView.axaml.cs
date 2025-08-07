2a862467e6740e7e166b1c4aa097f470c582f7d4
Wilbert van Dolleweerd <wilbert@arentheym.com>
2025-08-07 16:46:58 +0200

    Replace Avalonia SimpleTheme with SukiUI Theme

--- src/UI/Views/MainView.axaml.cs
+++ src/UI/Views/MainView.axaml.cs
@@ -1,10 +1,11 @@
 using Arentheym.ParkingBarrier.UI.ViewModels;
 using Avalonia.Controls;
 using CommunityToolkit.Mvvm.DependencyInjection;
+using SukiUI.Controls;
 
 namespace Arentheym.ParkingBarrier.UI.Views;
 
-public partial class MainView : Window
+public partial class MainView : SukiWindow
 {
     public MainView()
     {
